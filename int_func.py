55f3ed9b09c7f6be58be8600867e86d9efa811ae
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-13 12:26:06 +0530

    str_int_float

---
+++ int_func.py
@@ -0,0 +1,17 @@
+# int function
+# number_one = int(input("Enter first number "))
+# number_two = int(input("Enter second number "))
+
+# print(type(number_one), type(number_two))
+
+# total = number_one + number_two
+
+# print("total is " + str(total))
+
+
+num1 = str(4)
+num2 = float("44")
+num3 = int("33")
+print(num2)
+print(num3)
+print(num2 + num3)
