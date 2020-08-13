ed1c59728db0dd59c393aa4f0deb304eb9e223f7
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-13 11:47:48 +0530

    variables

--- variable.py
+++ variable.py
@@ -1,1 +1,25 @@
 # Variables
+
+# number1 = 2
+# print(number1)
+# number1 = 4
+# print(number1)
+
+# String, Number
+# name = 'Deva'
+# print(name)
+# name = 123
+# print(name)
+
+
+# Naming conventions for Variable
+'''
+we can't start a variable name with a number
+we can't give a space in between the variable name
+we can only allowed to use letters, numbers and underscore(_) for Naming the variable
+'''
+num1 = 1
+_greet = 'Hello'
+first_name = 'vasudev'  # Snake case writing
+print(num1, first_name, _greet)
+print()
