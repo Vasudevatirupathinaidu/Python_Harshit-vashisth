65995aebdaac25216f2c824793b36d8d36e82f22
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-13 12:03:12 +0530

    user_input

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# User Input
+# input function
+name = input("Type your name ")
+print("Hello " + name)
+print()
+
+# input function returns string
+age = input("What is your age? ")
+print("My age is " + age)
