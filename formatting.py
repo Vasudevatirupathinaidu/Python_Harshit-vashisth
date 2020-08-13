b1d6e78e61e15a075dbd089a0df8699af93f64dc
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-13 13:06:45 +0530

    string_formatting

---
+++ formatting.py
@@ -0,0 +1,11 @@
+name = 'Vasudev'
+age = 24
+
+print("Hello " + name + " your age is " + str(age))  # ugly syntex
+print()
+
+print("Hello {} your age is {}".format(name, age))  # python 3
+print()
+
+print(f"Hello {name} your age is {age}") # python 3.6
+print()
