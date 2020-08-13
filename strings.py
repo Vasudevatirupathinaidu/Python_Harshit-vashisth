ab7c889117946ad2a15f7f0df655dc18d1fa9376
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-13 11:56:58 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+# Strings
+'''
+A collection of characters inside single and double quotes
+'''
+
+first_name = "Vasudev"
+last_name = "Bonu"
+full_name = first_name + " " + last_name
+# print(full_name)
+# print(first_name + 1)  # Error
+# print(first_name + "1")
+# print(first_name + str(1))
+
+print((first_name + ' ') * 3)
+print()
