1749352aa23231f4763a44fb44557ee0beee8dc0
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-18 12:13:11 +0530

    list functions, nested list

---
+++ 105_min_max.py
@@ -0,0 +1,12 @@
+# min and max functions
+
+numbers = [6, 60, 2]
+
+print(min(numbers))
+print(max(numbers))
+
+words = ['vasu', 'deva']
+print(min(words))
+print(max(words))
+print(ord('v'))
+print(ord('d'))
---
+++ 93_list_inside_list.py
@@ -0,0 +1,13 @@
+# list inside list
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# 3 items ---> 3 list
+# print(matrix[1])
+# print(matrix[1][0])
+
+# for i in matrix:
+#     print(i)
+
+# for sublist in matrix:
+#     for i in sublist:
+#         print(i)
+
