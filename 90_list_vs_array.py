fc0a7bef7ecb7a3898019b859c2fbac929422ff0
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-18 09:05:18 +0530

    list vs array

---
+++ 90_list_vs_array.py
@@ -0,0 +1,10 @@
+# List vs Array
+
+# c, c++, java
+# array - fix data type (int, string)
+
+# list - store any data / flexible
+# python array module - fix data type
+# numpy arrays - binding with c libraries
+
+# javascript array = python list / flexible
