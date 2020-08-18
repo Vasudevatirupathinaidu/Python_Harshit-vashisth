283d2f6c57d3dd770cb8b832ae1683c39bf1c70e
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-18 08:45:09 +0530

    join, split method

---
+++ 89_join_split_method.py
@@ -0,0 +1,14 @@
+# split method
+# convert string to list
+user_info = "Vasu,26".split(',')
+print(user_info)
+
+# name, age = input("Enter your name and age ").split(',')
+# print(name)
+# print(age)
+
+
+# join method
+# convert list to string
+user_info = ['vasudev', '24']
+print(','.join(user_info))
