2abb1ccdd34e6ad07e7a67b7f27e849e424ffd11
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-18 09:12:35 +0530

    loop_list

---
+++ 92_loop_list.py
@@ -0,0 +1,13 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+# for loop
+for fruit in fruits:
+    print(fruit)
+print()
+
+
+# while loop
+i = 0
+while len(fruits) > i:
+    print(fruits[i])
+    i += 1
