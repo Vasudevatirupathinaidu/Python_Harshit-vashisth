6fd8e84d4188c1f6d7edf8043f40f55953a886e8
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-08-18 07:42:37 +0530

    list methods and functions

---
+++ 87_some_methods_list.py
@@ -0,0 +1,24 @@
+# count, sort method, sorted function, reverse, clear, copy
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+# print(fruits.count('apple'))
+
+# fruits.sort()
+# print(fruits)
+
+numbers = [3, 5, 1, 2, 9, 10]
+# numbers.sort()
+# print(numbers)
+# print(sorted(numbers))
+# print(numbers)
+
+# numbers.clear()
+# print(numbers)
+
+numbers_copy = numbers.copy()
+# print(numbers_copy)
+
+numbers_copy[0] = 100
+print(numbers_copy)
+print(numbers)
