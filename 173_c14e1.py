42cbca171b0544f8e72a2285c81abc4d18c85585
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2021-01-21 12:17:51 +0530

    Update 173_c14e1.py
    example of decorators

--- 173_c14e1.py
+++ 173_c14e1.py
@@ -1,5 +1,20 @@
 # exercise decorator
+import time
+from functools import wraps
 
-print('This is line one')
-x = 5
+def calculate_time(func):
+  @wraps(func)
+  def wrapper_func(*args, **kwargs):
+    t1 = time.time()
+    returned_value = func(*args, **kwargs)
+    t2 = time.time() - t1
+    print(f"{func.__name__} function took {t2:5f} sec to run")
+    return returned_value
+  return wrapper_func
 
+
+@calculate_time
+def square_funder(n):
+  return [i**2 for i in range(n)]
+
+square_funder(10000)
